dbe6ae4a325fab04a3aa334bdf37de4737b91ecc
agill671 <182363750+agill671@users.noreply.github.com>
2025-06-23 13:57:19 +0100

    Correction to animal_sounds documentation.

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -4,8 +4,8 @@
 #'
 #' Is a wrapper to [paste0()]. This is a *silly* function in a **toy package**.
 #'
-#' @param animal A **string** containing an animal name. Must be longer than 1 character.
-#' @param sound A **string** containing the sound that the animal makes. Must be longer than 1 character.
+#' @param animal A **string** containing an animal name.
+#' @param sound A **string** containing the sound that the animal makes.
 #'
 #' @returns A **string** in the form "The \{animal\} says \{sound\}!"
 #' @export
