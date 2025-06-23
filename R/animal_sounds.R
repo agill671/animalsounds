a265276d6aad4096020d17206b5a18c0b7357112
agill671 <gillamy671@gmail.com>
2025-06-23 11:57:08 +0100

    Add animal_sounds function

---
+++ R/animal_sounds.R
@@ -0,0 +1,8 @@
+animal_sounds <- function(animal, sound) {
+  stopifnot(is.character(animal) & length(animal) == 1)
+  stopifnot(is.character(sound) & length(sound) == 1)
+  paste0("The ", animal, " says ", sound, "!")
+}
+
+
+
