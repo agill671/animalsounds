711149ff5eaec5e0df7226910266c0317b9d6e7f
agill671 <182363750+agill671@users.noreply.github.com>
2025-06-23 15:21:07 +0100

    test animal_sounds

---
+++ tests/testthat/test-animal_sounds.R
@@ -0,0 +1,10 @@
+test_that("animal_sounds produces expected strings", {
+  dog_woof <- animal_sounds("dog", "woof")
+  expect_equal(dog_woof, "The dog says woof!")
+  expect_equal(animal_sounds("cat", "miaow"), "The cat says miaow!")
+})
+
+test_that("handles invalid inputs", {
+  expect_error(animal_sounds("dog", c("woof", c("bow wow wow"))),
+               class = "error_not_single_string")
+})
