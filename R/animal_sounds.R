eb5a4eba051021ed9ad0b57bf09536ee6c990850
agill671 <182363750+agill671@users.noreply.github.com>
2025-06-24 10:45:24 +0100

    add check_arg function

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -14,29 +14,26 @@
 #' animal_sounds("dog", "woof")
 animal_sounds <- function(animal, sound = NULL) {
 
-  if (!rlang::is_character(animal, n = 1)) {
-    cli::cli_abort(
-      c("{.var animal} must be a single string!",
-        "i" = "It was {.type {animal}} of length {length(animal)} instead."),
-      class = "error_not_single_string"
-    )
-  }
+  check_arg(animal)
 
   if (is.null(sound)) {
     return(paste0("The ", animal, " makes no sound."))
   }
 
-  if (!rlang::is_character(sound, n = 1)) {
-    cli::cli_abort(
-      c("{.var sound} must be a single string!",
-        "i" = "It was {.type {sound}} of length {length(sound)} instead."),
-      class = "error_not_single_string"
-    )
-  }
+  check_arg(sound)
 
   paste0("The ", animal, " says ", sound, "!")
 
 }
 
+check_arg <- function(arg, n = 1) {
+  if (!rlang::is_character(arg, n = n)) {
+    cli::cli_abort(
+      c("{.var {rlang::caller_arg(arg)}} must be a single string!",
+        "i" = "It was {.type {arg}} of length {length(arg)} instead."),
+      class = "error_not_single_string"
+    )
+  }
+}
 
 
